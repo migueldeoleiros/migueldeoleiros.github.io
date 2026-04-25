242c9e4aa39fe5241b34fbe1bbd6998471323577
migueldeoleiros <migueldeoleiros@gmail.com>
2026-04-25 19:57:53 +0200

    Configure domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [icon()],
+  site: 'https://migueldeoleiros.com',
+  integrations: [icon()],
 });
